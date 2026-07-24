6de74d4f8ca3f2066b84422286fbdeddc38c7139
Ardelan Yamanel <ardelanyamanel@outlook.de>
2026-07-24 13:35:00 +0200

    chore(scripts/activity): add total member count

--- src/scripts/activity.ts
+++ src/scripts/activity.ts
@@ -5,8 +5,18 @@ export default async function activity() {
 
   const guilds = await client.guilds.fetch();
 
+  let totalMembers = 0;
+
+  for (const cachedGuild of guilds.values()) {
+    const guild = await cachedGuild.fetch();
+
+    if (!guild) continue;
+
+    totalMembers += guild.memberCount;
+  }
+
   client.user.setActivity({
-    name: `Auf ${guilds.size} Discord-Servern`,
+    name: `Auf ${guilds.size} Discord-Servern. Erreicht ${totalMembers} Mitglieder.`,
     state: 'made by ardelan869'
   });
 }
